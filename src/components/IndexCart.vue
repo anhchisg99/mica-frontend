6bd159c27fee3c9bf62221424f0b99a79bc109ac
anhchisg99 <anhchictw311@gmaiil.com>
2023-04-07 18:31:35 +0700

    redesign button and secure_url

--- src/components/IndexCart.vue
+++ src/components/IndexCart.vue
@@ -38,15 +38,15 @@
                   <td class="cart-td">{{ item.quantity }}</td>
                   <td class="cart-td">${{ item.price }}</td>
                 </tr>
-                <tr>
-                  <td colspan="2" class="cart-td cart-coupon">
+                <tr class="">
+                  <td colspan="2" class="cart-td cart-coupon cart-secure_url">
                     <!-- Alfreds Futterkiste -->
                     <!-- <a @click="deleteItemFromCart(item)"><ion-icon name="close-outline"></ion-icon></a> -->
                     <img v-if="secure_url" :src="secure_url" alt="" />
                     <!-- {{ popup_text }} -->
                   </td>
 
-                  <td colspan="3" update-button class="update-button">
+                  <td colspan="3" update-button class="update-button cart-secure_url">
                     <a class="btn" @click="openUploadWidget()">Tải hình ảnh</a>
                   </td>
                 </tr>
