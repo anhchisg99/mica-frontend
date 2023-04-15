7cc53f4d4861ea480ce1654b67ea3543ea50060a
anhchisg99 <anhchictw311@gmaiil.com>
2023-04-15 17:00:47 +0700

    fix payment success

--- src/components/Success.vue
+++ src/components/Success.vue
@@ -6,7 +6,8 @@
                     <div class="success-payment-img">
                         <p><img src="../assets/success-1.png" alt="" /></p>
                     </div>
-                    <h3>THANH TOÁN THÀNH CÔNG !</h3>
+                    <h3>Đặt Hàng Thành Công !</h3>
+                    <h4>Đơn hàng sẽ giao trong 1,2 ngày tới kể từ lúc bắt đầu đặt hàng !</h4>
                     <div class="success-payment-button">
                         <a href="" @click="goToHome()">TRỞ VỀ TRANG CHỦ</a>
                     </div>
